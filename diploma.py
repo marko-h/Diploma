d7054968f2dd8cb1bae6663e9fddfabc314863f4
Marko Horvat <marko.horvat077@gmail.com>
2025-04-25 17:26:43 +0200

    Added datasets

---
+++ diploma.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, precision_score, recall_score, f1_score
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.svm import SVC, SVR
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+
+
